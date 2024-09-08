54e9854b90b54b8fc8efab7797be7edeac05235d
ShantanuSaxena17 <148350285+ShantanuSaxena17@users.noreply.github.com>
2024-09-08 12:53:23 +0530

    Script and style files

---
+++ script.js
@@ -0,0 +1,54 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+
+const location_not_found = document.querySelector('.location-not-found');
+const weather_body = document.querySelector('.weather-body');
+
+async function checkWeather(city){
+
+    const api_key = "c8439e441ab1f82fe6b28b91f4602406";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+    const weather_data = await fetch(`${url}`).then(response => response.json());
+
+    if(weather_data.cod === `404`){
+        location_not_found.style.display = "flex";
+        weather_body.style.display = "none";
+        return;
+    }
+
+    location_not_found.style.display = "none";
+    weather_body.style.display = "flex";
+
+    temperature.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+    description.innerHTML = `${weather_data.weather[0].description}`;
+
+
+
+    switch(weather_data.weather[0].main){
+        case 'Clouds':
+            weather_img.src = "images/cloud.png";
+            break;
+        case 'Clear':
+            weather_img.src = "images/clear.png";
+            break;
+        case 'Rain':
+            weather_img.src = "images/rain.png";
+            break;
+        case 'Mist':
+            weather_img.src = "images/mist.png";
+            break;
+        case 'Snow':
+            weather_img.src = "images/snow.png";
+            break;
+
+    }
+}
+
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(inputBox.value);
+});
